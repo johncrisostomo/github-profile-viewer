5859c373d0d7aafcc4ac41d78676630cbbf0bc45
John Crisostomo <johncrisostomo@protonmail.ch>
2017-06-14 17:26:30 +0800

    some silly style experiments

--- screens/Profile.js
+++ screens/Profile.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import _ from 'underscore';
-import { Avatar } from 'react-native-elements';
+import { Avatar, Divider } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { Text, View, ActivityIndicator } from 'react-native';
 
@@ -18,9 +18,13 @@ class Profile extends Component {
               source={{ uri: avatar_url }}
             />
             <View>
-              <Text style={{ fontSize: 30, fontWeight: 'bold', marginTop: 15 }}>{name}</Text>
-              <Text style={{ fontSize: 12,  alignSelf: 'center' }}>{login}</Text>
+              <Text style={[{ fontSize: 30, fontWeight: 'bold', marginTop: 15 }, { color: '#F1F1F1' }]}>{name}</Text>
+              <Text style={[{ fontSize: 12,  alignSelf: 'center' }, { color: '#F1F1F1' }]}>{login}</Text>
             </View>
+          </View>
+          <Divider style={{ backgroundColor: '#F1F1F1' }} />
+          <View style={styles.followersContainer}>
+            <Text style={[styles.followersHeader, { color: '#F1F1F1' }]}>FOLLOWERS</Text>
           </View>
         </View>
       );
@@ -41,12 +45,22 @@ const mapStateToProps = ({ github }) => {
 const styles = {
   containerStyle: {
     flex: 1,
+    backgroundColor: '#000',
   },
   headerStyle: {
     flexDirection: 'row',
     justifyContent: 'space-around',
     marginTop: 20,
     padding: 10,
+  },
+  followersContainer: {
+
+  },
+  followersHeader:  {
+    alignSelf: 'center',
+    marginTop: 10,
+    fontSize: 20,
+    fontWeight: 'bold'
   },
 };
 
